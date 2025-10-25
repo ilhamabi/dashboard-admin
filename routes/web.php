4628c32e9ad96520f4ccf2b425f70c623afddcff
ilhamabi <33386487+ilhamabi@users.noreply.github.com>
2025-10-25 15:30:13 +0700

    Add dark mode, change color of dashboard, change navbar

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/login', function () {
+    return view('login');
+})->middleware(['auth', 'verified'])->name('login');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
